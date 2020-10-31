9b5e0cf2d3e683a5bbf2bcc332914b0da4270661
LuisMLeonorITCA <64717459+LuisMLeonorITCA@users.noreply.github.com>
2020-10-31 01:58:30 -0600

    Commit - Personalizaciones minimas y del Fragmento Home

---
+++ app/src/main/java/com/lab3/lab3sqlvolley/ui/categorias/categorias.java
@@ -0,0 +1,21 @@
+package com.lab3.lab3sqlvolley.ui.categorias;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lab3.lab3sqlvolley.R;
+
+public class categorias extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_categorias, container, false);
+    }
+}
