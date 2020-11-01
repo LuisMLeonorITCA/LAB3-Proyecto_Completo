29c76105593f643440d1dce1d4d5c01ac65cb44b
LuisMLeonorITCA <64717459+LuisMLeonorITCA@users.noreply.github.com>
2020-11-01 15:33:35 -0600

    Commit -  Diseño y definicion del Fragmento y clase producto

--- app/src/main/java/com/lab3/lab3sqlvolley/Setting_VAR.java
+++ app/src/main/java/com/lab3/lab3sqlvolley/Setting_VAR.java
@@ -5,4 +5,8 @@ public class Setting_VAR {
     public static final String URL_GUARDAR_CATEGORIAS ="http://192.168.100.5/Service2020/guardar_categorias.php";
     public static final String URL_CONSULTAR_CATEGORIAS ="http://192.168.100.5/Service2020/searchAllCategoria.php";
 
+    public static final String URL_guardar_categoria ="http://192.168.100.5/Service2020/guardar_categorias.php";
+    public static final String URL_consultaAllCategorias ="http://192.168.100.5/Service2020/buscar_Categorias.php";
+    public static final String URL_registrar_categorias ="http://192.168.100.5/Service2020/guardar_categorias.php";
+    public static final String URL_registrar_productos ="http://192.168.100.5/Service2020/guardar_Categorias.php";
 }
